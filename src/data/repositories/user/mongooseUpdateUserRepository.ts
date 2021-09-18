4f2dffee81af8f68db8a51cf4342b80892247e68
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 14:26:48 -0300

    Adding update user repo mongoose's implementation

---
+++ src/data/repositories/user/mongooseUpdateUserRepository.ts
@@ -0,0 +1,32 @@
+import DomainError from "../../../domain/errors/domainError";
+import RepositoryError from "../../../domain/errors/repositoryError";
+import UpdateUserRepository from "../../../domain/repositories/user/updateUserRepository";
+import MongooseService from "../../../external/mongoose/mongooseService";
+import UserDto from "../../dtos/user/userDto";
+import UserSchema from '../../../external/mongoose/schemas/user';
+
+class MongooseUpdateUserRepository implements UpdateUserRepository{
+
+    constructor(private mongoose : MongooseService){}
+
+    async call(data: UserDto): Promise<UserDto | DomainError> {
+        let result;
+        data.updatedAt = new Date();
+        try {
+            result = await UserSchema.findOneAndUpdate(
+                {
+                    email: data.email,
+                },
+                data
+            );
+        } catch (error) {
+            result = new RepositoryError('Houve um erro ao atualizar seu usuário',error,400);
+        }
+        this.mongoose.closeConnection();
+        return result;
+    }
+
+    
+}
+
+export default MongooseUpdateUserRepository;
