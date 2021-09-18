ec49072948b72c20e98af7a2a57d9d17b1531eaa
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 16:16:09 -0300

    Fix Login Repository return types

--- src/domain/repositories/auth/loginRepository.ts
+++ src/domain/repositories/auth/loginRepository.ts
@@ -1,9 +1,9 @@
 import AuthDto from "../../../data/dtos/auth/authDto";
-import ResponseAuthDto from "../../../data/dtos/auth/responseAuthDto";
+import UserDto from "../../../data/dtos/user/userDto";
 import DomainError from "../../errors/domainError";
 
 interface LoginRepository{
-    call(data: AuthDto) : Promise<ResponseAuthDto|DomainError>
+    call(data: AuthDto) : Promise<UserDto|DomainError|null>
 }
 
 export default LoginRepository;
