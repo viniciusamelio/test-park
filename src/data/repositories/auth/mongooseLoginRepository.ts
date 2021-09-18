751287a25a47a43b40c303915125c3b4128837ff
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 16:16:37 -0300

    Adding Mongoose Login repository's implementation

---
+++ src/data/repositories/auth/mongooseLoginRepository.ts
@@ -0,0 +1,24 @@
+import DomainError from "../../../domain/errors/domainError";
+import LoginRepository from "../../../domain/repositories/auth/loginRepository";
+import AuthDto from "../../dtos/auth/authDto";
+import UserSchema from '../../../external/mongoose/schemas/user';
+import RepositoryError from "../../../domain/errors/repositoryError";
+import UserDto from "../../dtos/user/userDto";
+
+class MongooseLoginRepository implements LoginRepository{
+    async call(data: AuthDto): Promise< UserDto | DomainError| null> {
+        let result;
+        try {
+            const user = await UserSchema.findOne({
+                email: data.email
+            });
+            result =  user;
+        } catch (error) {
+            result = new RepositoryError('Não conseguimos te logar',error,401);
+        }
+        return result;
+    }
+
+}
+
+export default MongooseLoginRepository;
