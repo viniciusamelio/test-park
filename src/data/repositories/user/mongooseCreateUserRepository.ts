b24e1902fd27c750b7bc77a80d20fedadbfb6080
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 14:14:28 -0300

    Adding closeConnection call to create user repo

--- src/data/repositories/user/mongooseCreateUserRepository.ts
+++ src/data/repositories/user/mongooseCreateUserRepository.ts
@@ -6,7 +6,7 @@ import UserDto from "../../dtos/user/userDto";
 import UserSchema from '../../../external/mongoose/schemas/user';
 
 class MongooseCreateUserRepository implements CreateUserRepository{
-    constructor(mongoose: MongooseService){}
+    constructor(private mongoose: MongooseService){}
 
     async call(data: UserDto): Promise<UserDto | DomainError> {
         let result;
@@ -15,6 +15,7 @@ class MongooseCreateUserRepository implements CreateUserRepository{
         } catch (error) {
             result = new RepositoryError('Houve um erro ao salvar o usuário',error,500);
         }
+        this.mongoose.closeConnection();
         return result;
     }
 
