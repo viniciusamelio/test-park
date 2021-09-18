d1a47fc496992b86af458ab61589f7c9c3423512
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 16:26:12 -0300

    Fix Login usecase

--- src/domain/usecases/auth/loginUsecase.ts
+++ src/domain/usecases/auth/loginUsecase.ts
@@ -1,12 +1,20 @@
 import AuthDto from "../../../data/dtos/auth/authDto";
+import ResponseAuthDto from "../../../data/dtos/auth/responseAuthDto";
+import AuthError from "../../errors/authError";
+import DomainError from "../../errors/domainError";
 import LoginRepository from "../../repositories/auth/loginRepository";
+import EncryptService from "../../services/encryptService";
 
 class LoginUsecase{
-    constructor(private repository: LoginRepository){}
+    constructor(private repository: LoginRepository, private hashService: EncryptService){}
 
-    async call(data:AuthDto){
+    async call(data:AuthDto) : Promise<AuthError|DomainError|ResponseAuthDto>{
         const result = await this.repository.call(data);
-        return result;
+        if(!result) return new AuthError();
+        if(result instanceof DomainError) return result;
+        const isValidAuth = this.hashService.compare(data.password, result.password);
+        if(!isValidAuth) return new AuthError();
+        return {message: "Logado com sucesso!"};
     }
 }
 
