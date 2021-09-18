40ac59ba3ddf9a687721768c4e89b29cf6fd62d7
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-17 22:11:27 -0300

    Adding status code property to invalid entity error

--- src/domain/errors/invalidEntity.ts
+++ src/domain/errors/invalidEntity.ts
@@ -3,9 +3,11 @@ import DomainError from "./domainError";
 class InvalidEntityError implements DomainError{
     message: String;
     stackTrace?: any;
+    statusCode:Number;
     constructor(message:String,stackTrace:any){
         this.message = message;
         this.stackTrace = stackTrace;
+        this.statusCode = 400;
     }
 }
 
