6b1daf398d5cff8d4430817be87bd180d37ff433
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 15:43:10 -0300

    Adding find user by id usecase to usercontroller

--- src/presentation/controllers/user/userController.ts
+++ src/presentation/controllers/user/userController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import MongooseCreateUserRepository from "../../../data/repositories/user/mongooseCreateUserRepository";
 import MongooseFindUserByEmailRepository from "../../../data/repositories/user/mongooseFindUserByEmailRepository";
+import MongooseFindUserByIdRepository from "../../../data/repositories/user/mongooseFindUserByIdRepository";
 import MongooseListUserRepository from "../../../data/repositories/user/mongooseListUserRepository";
 import MongooseUpdateUserRepository from "../../../data/repositories/user/mongooseUpdateUserRepository";
 import User from "../../../domain/entities/user";
 import DomainError from "../../../domain/errors/domainError";
 import CreateUserUsecase from "../../../domain/usecases/user/createUserUsecase";
 import FindUserByEmailUsecase from "../../../domain/usecases/user/findUserByEmailUsecase";
+import FindUserByIdUsecase from "../../../domain/usecases/user/findUserByIdUsecase";
 import ListUserUsecase from "../../../domain/usecases/user/listUserUsecase";
 import UpdateUserUseCase from "../../../domain/usecases/user/updateUserUsecase";
 import BcryptService from "../../../external/bcrypt/bcryptService";
@@ -19,6 +21,8 @@ class UserController{
     private updateUserUseCase : UpdateUserUseCase = new UpdateUserUseCase( new MongooseUpdateUserRepository(this.mongooseService), new BcryptService() );
     private listUserUseCase : ListUserUsecase = new ListUserUsecase( new MongooseListUserRepository(this.mongooseService) );
     private findUserByEmailUseCase : FindUserByEmailUsecase = new FindUserByEmailUsecase( new MongooseFindUserByEmailRepository(this.mongooseService) );
+    private findUserByIdUseCase : FindUserByIdUsecase = new FindUserByIdUsecase( new MongooseFindUserByIdRepository(this.mongooseService) );
+
     createUser = async(request:Request, response:Response) => {
         try {
             const data = request.body;
@@ -71,6 +75,21 @@ class UserController{
         try {
             this.user = new User();
             const result = await this.user.getByEmail(this.findUserByEmailUseCase, request.params.email);
+            if(result instanceof DomainError){
+                return response.status(result.statusCode!).json(result);
+            }
+            return response.json(result);
+        } catch (error) {
+            let statusCode = 500;
+            if(error instanceof DomainError) statusCode = 400;
+            return response.status(statusCode).json(error);
+        }  
+    }
+
+    findUserById = async(request:Request, response:Response) => {
+        try {
+            this.user = new User();
+            const result = await this.user.getById(this.findUserByIdUseCase, request.params.id);
             if(result instanceof DomainError){
                 return response.status(result.statusCode!).json(result);
             }
