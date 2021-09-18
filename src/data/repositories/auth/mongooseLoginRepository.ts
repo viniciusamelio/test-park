2c7e18311e28c531f0550a47478aff5ef905f07b
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-18 16:34:23 -0300

    Adding connection handling to login repo

--- src/data/repositories/auth/mongooseLoginRepository.ts
+++ src/data/repositories/auth/mongooseLoginRepository.ts
@@ -4,11 +4,14 @@ import AuthDto from "../../dtos/auth/authDto";
 import UserSchema from '../../../external/mongoose/schemas/user';
 import RepositoryError from "../../../domain/errors/repositoryError";
 import UserDto from "../../dtos/user/userDto";
+import MongooseService from "../../../external/mongoose/mongooseService";
 
 class MongooseLoginRepository implements LoginRepository{
+    constructor(private mongoose: MongooseService){}
     async call(data: AuthDto): Promise< UserDto | DomainError| null> {
         let result;
         try {
+            this.mongoose.openConnection();
             const user = await UserSchema.findOne({
                 email: data.email
             });
@@ -16,6 +19,7 @@ class MongooseLoginRepository implements LoginRepository{
         } catch (error) {
             result = new RepositoryError('Houve um erro ao tentar te logar',error,500);
         }
+        this.mongoose.closeConnection();
         return result;
     }
 
