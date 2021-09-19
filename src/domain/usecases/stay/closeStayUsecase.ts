3d15edf731b6d494ad06a418b0b0afd4adeb5633
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-19 12:19:45 -0300

    Adding close stay usecase

---
+++ src/domain/usecases/stay/closeStayUsecase.ts
@@ -0,0 +1,17 @@
+import StayDto from "../../../data/dtos/stay/stayDto";
+import DomainError from "../../errors/domainError";
+import FindStayOpenedRepository from "../../repositories/stay/findOpenedStayRepository";
+import UpdateStayRepository from "../../repositories/stay/updateStayRepository";
+
+class CloseStayUsecase{
+    constructor(private updateStayRepository: UpdateStayRepository, private findOpenedStayRepository : FindStayOpenedRepository){}
+    async call(idUser:string) : Promise<StayDto|DomainError>{
+        let openedStayOrError = await this.findOpenedStayRepository.call(idUser);
+        if(openedStayOrError instanceof DomainError) return openedStayOrError;
+        openedStayOrError.end = new Date();
+        const result = await this.updateStayRepository.call(openedStayOrError);
+        return result;
+    }
+}
+
+export default CloseStayUsecase;
