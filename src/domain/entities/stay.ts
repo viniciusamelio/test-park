c6061e8354136723e5b7cdd574d33fdc800917a3
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-19 14:33:20 -0300

    Adding usecases to stay entity

--- src/domain/entities/stay.ts
+++ src/domain/entities/stay.ts
@@ -1,5 +1,9 @@
 import StayDto from "../../data/dtos/stay/stayDto";
 import InvalidEntityError from "../errors/invalidEntity";
+import CloseStayUsecase from "../usecases/stay/closeStayUsecase";
+import CreateStayUsecase from "../usecases/stay/createStayUsecase";
+import ListStayUsecase from "../usecases/stay/listStayUsecase";
+import UpdateStayUsecase from "../usecases/stay/updateStayUsecase";
 
 class Stay{
     constructor(public data?:StayDto){
@@ -19,6 +23,26 @@ class Stay{
         if(!this.data?.licensePlate || this.data?.licensePlate.length < 7){
             throw new InvalidEntityError('Placa do veículo inválida.');
         }
+    }
+
+    create = async(usecase: CreateStayUsecase)=>{
+        const result = await usecase.call(this.data!);
+        return result;
+    }
+
+    update = async(usecase: UpdateStayUsecase)=>{
+        const result = await usecase.call(this.data!);
+        return result;  
+    }
+
+    list = async(usecase: ListStayUsecase)=>{
+        const result = await usecase.call(this.data!.user);
+        return result;  
+    }
+
+    close = async(usecase: CloseStayUsecase)=>{
+        const result = await usecase.call(this.data!.user);
+        return result;  
     }
 
 }
