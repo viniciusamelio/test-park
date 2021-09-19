9d59aac9d8bd527eca173add4f66b87023865580
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-19 11:58:28 -0300

    Find search logic

--- src/domain/repositories/stay/findStayRepository.ts
+++ src/domain/repositories/stay/findStayRepository.ts
@@ -2,7 +2,7 @@ import StayDto from "../../../data/dtos/stay/stayDto";
 import DomainError from "../../errors/domainError";
 
 interface FindStayRepository{
-    call(data: StayDto, userId: string):Promise<StayDto[]|DomainError>;
+    call( userId: string):Promise<StayDto[]|DomainError>;
 }
 
 export default FindStayRepository;
